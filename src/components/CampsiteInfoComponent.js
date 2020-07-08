e1a737000c406f2d8eff1c4bc031000c4d6a676b
Charles Steck <hannonow@gmail.com>
2020-07-08 15:09:49 -0500

    functional components

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCampsite({campsite}) {
+
+}
+
+function RenderComments({coments}) {
+}
+
+function CampsiteInfo(props) {
+  if(props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+            <RenderCampsite campsite={props.campsite} />
+            <RenderComments comments={props.campsite.comments} />
+        </div>
+      </div>
+    );
+  }
+  return <div />;
+}
+ 
+export default CampsiteInfo;
