1864ade1d5b1f6fd5210c28db82fca74f5e5e9e6
Charles Steck <hannonow@gmail.com>
2020-08-16 12:16:17 -0500

    react router parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -51,15 +51,19 @@ function Directory(props) {
 */
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 
 function RenderDirectoryItem({campsite}) {
   return (
     <Card>
+       <Link to={`/directory/${campsite.id}`}>
     <CardImg width="100%" src={campsite.image} alt={campsite.name} />
    <CardImgOverlay>
    <CardTitle>{campsite.name}</CardTitle>
    </CardImgOverlay>
+   </Link>
      </Card>
   );
 }
